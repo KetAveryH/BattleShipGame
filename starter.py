1e8012ff0abbbd75ee74ac2f6d659044a8e37cb7
KetAveryH <khollingsworth@g.hmc.edu>
2021-12-01 22:49:29 -0800

    This is the Main, what we have so far

--- starter.py
+++ starter.py
@@ -1,8 +1,9 @@
 """
 To begin we will want to start with the creation of an 8x8 grid
 """
-
+blue = '\u2B1C'
 class Board:
+    
 
     def __init__(self, width, height):
         """Construct objects of type Board, with the given width and height."""
@@ -23,20 +24,50 @@ class Board:
             s += " "
             s += alphabet[col]
         s += '\n'  + ' ' 
-        s += '\u2B1C'*(self.width + 2) + '\n'                        
+        s += blue*(self.width + 2) + '\n'                        
         
         for row in range(1, self.height+1):
             s += str(row-1)
-            s += '\u2B1C'
+            s += blue
             for col in range(0, self.width):
                 s += self.data[row-1][col] + ''
             s += "\u2B1C"
             s += '\n'
         s += ' '
-        s += (self.width +1) * '\u2B1C'   # Bottom of the board
-        s += '\u2B1C'
+        s += (self.width +1) * blue   # Bottom of the board
+        s += blue
 
         return s       # Return the Board
+
+    def allowsMove(self, col, row, l, ori):
+        #Up Orientation
+        if not( 0 <= row and row <= self.height) or not( 0 <= col and col <= self.width):
+            return False
+            return False
+        if ori == 'up':
+            if (row+1)-l < 0: 
+                return False #Here we will reprompt the player in a later function
+            for x in range(l):
+                if self.data[row-x][col] !=  blue:
+                    return False
+            else:
+                return True
+        if ori == 'down':
+            if (row)+l <= self.height: 
+                return False #Here we will reprompt the player in a later function
+            for x in range(l):
+                if self.data[row+x][col] !=  blue:
+                    return False
+            else:
+                return True
+        if ori == 'left':
+            if (col+1)-l < 0:
+                return False
+            for x in range(l):
+                if self.data[row][col-x] !=  blue:
+                    return False
+            
+
 
 
 
